ace1c483fd6a7b463c0b3d986b9dc36ce0569750
Your Name <you@example.com>
2024-02-14 16:45:04 +0100

    [#186896046] Simplify reporting setup. Send src in addition to coverage

--- lib/ruby_ci/simple_cov.rb
+++ lib/ruby_ci/simple_cov.rb
@@ -1,16 +1,7 @@
+require_relative "simple_cov/reporting"
+
 module RubyCI
   module SimpleCov
-    def self.included base
-      base.instance_eval do
-        def write_last_run(result)
-          ::SimpleCov::LastRun.write(result:
-            result.coverage_statistics.transform_values do |stats|
-              round_coverage(stats.percent)
-            end)
-
-          RubyCI.report_simplecov(result.to_json)
-        end
-      end
-    end
+    ::SimpleCov.send(:include, RubyCI::SimpleCov::Reporting)
   end
 end
