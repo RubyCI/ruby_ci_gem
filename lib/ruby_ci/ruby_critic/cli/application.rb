54bea106d9c0bbe883247f91b426c9bb3e17dc8b
Your Name <you@example.com>
2024-07-19 16:30:14 +0200

    [#187918667] Rubyci RubyCritic reporting

--- lib/ruby_ci/ruby_critic/cli/application.rb
+++ lib/ruby_ci/ruby_critic/cli/application.rb
@@ -10,6 +10,8 @@ module RubyCI
     module Cli
       class Application < ::RubyCritic::Cli::Application
         def execute
+          events = []
+          events << ['running_ruby_critic_run'.upcase, {}]
           status = super
 
           content = File.read('tmp/rubycritic/report.json')
@@ -29,8 +31,18 @@ module RubyCI
           end
 
           compressed_data = ::Base64.strict_encode64(Zlib::Deflate.deflate(report.to_json, 9))
-          RubyCI.report_ruby_critic(compressed_data, 'passed')
+          events << ['running_ruby_critic_exit_status'.upcase, { exitstatus: status, output: '', compressed_data: compressed_data }]
 
+          if ENV['RBCI_REMOTE_TESTS'] == 'true'
+            json_events = {
+              build_id: RubyCI.configuration.orig_build_id,
+              compressed_data: Base64.strict_encode64(Zlib::Deflate.deflate(JSON.fast_generate(events), 9)),
+            }
+      
+            RubyCI.send_events(json_events)
+          else
+            RubyCI.report_ruby_critic(compressed_data, 'passed')
+          end
           return status
         end
       end
