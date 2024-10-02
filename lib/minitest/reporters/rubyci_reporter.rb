02c41d8baecef1ac3d08429aca6fe832ef6fdb7f
Your Name <you@example.com>
2024-10-02 16:22:18 +0200

    Result debugging

--- lib/minitest/reporters/rubyci_reporter.rb
+++ lib/minitest/reporters/rubyci_reporter.rb
@@ -80,7 +80,7 @@ module Minitest
         id = ids[description]
         path = test_path(result.klass)
 
-        debug(result_status(result)[0].upcase)
+        debug(result_status(result).to_s[0].upcase)
 
         test_results[path]['1'][id][:end] = Minitest.clock_time
         test_results[path]['1'][id][:run_time] = test_results[path]['1'][id][:end] - test_results[path]['1'][id][:start]
